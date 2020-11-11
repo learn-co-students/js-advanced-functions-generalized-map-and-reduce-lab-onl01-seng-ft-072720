20abeda29e8af9ae19dbd198524e371d6c27525b
popkorn96 <mpaloma2014@gmail.com>
2020-11-10 18:20:34 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(src, change){
+    let array = []
+    for(let i = 0; i < src.length; i++){
+        let specialElement = src[i]
+        array.push(change(specialElement))
+    }
+    return array
+};
+function reduce(src, change, start){
+    let rest = (!!start) ? start : src[0]
+    let i = (!!start) ? 0 : 1
+    for(; i < src.length; i++){
+        rest = change(src[i], rest)
+    }
+    return rest
+}
