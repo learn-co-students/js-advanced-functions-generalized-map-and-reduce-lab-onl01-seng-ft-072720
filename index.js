3f9577787ce9d81717fdf985eddf9ee9ccbbdbfa
KerriLynne <kerri.l.ogorman@gmail.com>
2020-11-11 15:56:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+function map(sourceArray, callb) {
+    let arr = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        let element = sourceArray[i]
+        arr.push(callb(element))
+    }
+    return arr;
+}
+
+//need to understand this
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
