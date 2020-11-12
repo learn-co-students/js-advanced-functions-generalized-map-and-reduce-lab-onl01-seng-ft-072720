e24b773bcf360291bb7401a3049691368990eec2
acoomes33 <github email address>
2020-11-12 17:55:26 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+const map = function(array, cb){
+    let r = []
+    for (let i = 0; i < array.length; i++ ) {
+        r.push(cb(array[i]))
+      }
+    return r
+}
+
+const reduce = function(array, cb, start){
+    let r = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+    for (;i< array.length; i++){
+        r = cb(array[i], r)
+    }
+    return r
+}
